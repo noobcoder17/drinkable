66e57bd7abe335d4dbd32e4e47673e41b161dc4d
noobcoder17 <debnathakash8@gmail.com>
2020-08-29 10:11:15 +0530

    init commit

---
+++ android/app/src/main/java/com/drinkable/drinkable/MainActivity.java
@@ -0,0 +1,7 @@
+package com.drinkable.drinkable;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Drinkable',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Drinkable'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
